01e53f6de0cd473c7bcf43e3ec7075e3d65b8a8c
NitroLine <sergey.and.natasha@yandex.ru>
2023-10-25 23:13:52 +0500

    fix link

--- src/components/pages/my-list-page.tsx
+++ src/components/pages/my-list-page.tsx
@@ -1,5 +1,6 @@
 import {Film} from '../../types/film';
 import {FilmsList} from '../films/films-list';
+import {Link} from 'react-router-dom';
 
 type MyListPageProps = {
   films: Array<Film>;
@@ -10,11 +11,11 @@ export function MyListPage({films}:MyListPageProps){
     <div className="user-page">
       <header className="page-header user-page__head">
         <div className="logo">
-          <a href="main.html" className="logo__link">
+          <Link to={'/'} className="logo__link">
             <span className="logo__letter logo__letter--1">W</span>
             <span className="logo__letter logo__letter--2">T</span>
             <span className="logo__letter logo__letter--3">W</span>
-          </a>
+          </Link>
         </div>
 
         <h1 className="page-title user-page__title">My list <span className="user-page__film-count">{films.length}</span></h1>
@@ -38,11 +39,11 @@ export function MyListPage({films}:MyListPageProps){
 
       <footer className="page-footer">
         <div className="logo">
-          <a href="main.html" className="logo__link logo__link--light">
+          <Link to={'/'} className="logo__link logo__link--light">
             <span className="logo__letter logo__letter--1">W</span>
             <span className="logo__letter logo__letter--2">T</span>
             <span className="logo__letter logo__letter--3">W</span>
-          </a>
+          </Link>
         </div>
 
         <div className="copyright">
